aff6c6882a7251166c8546140ff367ea40338539
yjung <yjung@318c8218-2dab-440d-9263-6f70bac63e5b>
2016-11-08 15:27:57 +0000

    fixed dfs unit tests
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@193170 318c8218-2dab-440d-9263-6f70bac63e5b

--- cr2res/tests/cr2res_dfs-test.c
+++ cr2res/tests/cr2res_dfs-test.c
@@ -1,6 +1,5 @@
-/* $Id: cr2re_dfs-test.c,v 1.3 2013-09-13 08:16:32 yjung Exp $
- *
- * This file is part of the CR2RE Pipeline
+/*
+ * This file is part of the CR2RES Pipeline
  * Copyright (C) 2002,2003 European Southern Observatory
  *
  * This program is free software; you can redistribute it and/or modify
@@ -18,13 +17,6 @@
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02111-1307  USA
  */
 
-/*
- * $Author: yjung $
- * $Date: 2013-09-13 08:16:32 $
- * $Revision: 1.3 $
- * $Name: not supported by cvs2svn $
- */
-
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
@@ -35,14 +27,18 @@
 
 #include <stdlib.h>
 #include <string.h>
-
 #include <cpl.h>
+#include <cr2res_dfs.h>
 
-#include <cr2re_dfs.h>
+/*-----------------------------------------------------------------------------
+                                Functions prototypes
+ -----------------------------------------------------------------------------*/
+
+static const char * cr2res_test_frame_group_to_string(cpl_frame_group group) ;
 
 /*----------------------------------------------------------------------------*/
 /**
- * @defgroup cr2re_dfs_test  Unit test of cr2re_dfs
+ * @defgroup cr2res_dfs_test    Unit test of cr2res_dfs
  *
  */
 /*----------------------------------------------------------------------------*/
@@ -51,51 +47,21 @@
 
 /*----------------------------------------------------------------------------*/
 /**
-  @brief    Textual representation of CPL frame group
-  @param    group     to convert
-  @return   textual representation
+  @brief    Unit test of cr2res_dfs_set_groups
  */
 /*----------------------------------------------------------------------------*/
-static const char *
-frame_group_to_string(cpl_frame_group group)
+static void cr2res_test_dfs_set_groups(void)
 {
-    const char * self = "???"; /* Only used if cpl_frame_group is extended */
-
-    switch(group) {
-    case CPL_FRAME_GROUP_RAW:
-        self = CPL_FRAME_GROUP_RAW_ID;
-        break;
-    case CPL_FRAME_GROUP_NONE:
-        self = "NONE";
-        break;
-    case CPL_FRAME_GROUP_CALIB:
-        self = CPL_FRAME_GROUP_CALIB_ID;
-        break;
-    case CPL_FRAME_GROUP_PRODUCT:
-        self = CPL_FRAME_GROUP_PRODUCT_ID;
-        break;
-    default:
-        break;
-    }
-
-    return self;
-}
-
-/*----------------------------------------------------------------------------*/
-/**
-  @brief    Unit test of cr2re_dfs_set_groups
- */
-/*----------------------------------------------------------------------------*/
-static void test_set_groups(void)
-{
-    const char *const fctid = "test_set_groups";
-    const char *const test_subject = "cr2re_dfs_set_groups";
+    int i;
+    const int N = 3;
+    const char *const fctid = "cr2res_test_dfs_set_groups";
+    const char *const test_subject = "cr2res_dfs_set_groups";
     cpl_errorstate prestate = cpl_errorstate_get();
 
     /* Test with invalid input */
-    if (cr2re_dfs_set_groups(NULL) == 0) {
+    if (cr2res_dfs_set_groups(NULL) == 0) {
         cpl_msg_error(fctid, "Function %s did not fail on NULL input",
-                      test_subject);
+                test_subject);
         cpl_end();
         exit(EXIT_FAILURE);       
     }
@@ -103,89 +69,109 @@ static void test_set_groups(void)
     cpl_errorstate_set(prestate);
 
     /* Test with valid input */
-    {
-        /* Simulate data */
-        const int N = 3;
-        const char *const filename[] = {"raw1.fits", 
-                                        "raw2.fits", 
-                                        "calib.fits"};
-        const char *const tag[] = {CR2RE_BIAS_RAW,
-                                   CR2RE_BIAS_RAW, 
-                                   CR2RE_BIAS_PROCATG};
-        cpl_frame_group const expected_group[] = {CPL_FRAME_GROUP_RAW,
-                                                  CPL_FRAME_GROUP_RAW,
-                                                  CPL_FRAME_GROUP_CALIB};
-        cpl_frameset *frames = cpl_frameset_new();
-        int i;
+    /* Simulate data */
+    const char *const filename[] = {
+        "raw1.fits", 
+        "raw2.fits", 
+        "calib.fits"
+    };
+    const char *const tag[] = {
+        CR2RES_DARK_RAW, 
+        CR2RES_DARK_RAW, 
+        CR2RES_MASTER_DARK_PROCATG
+    };
+    cpl_frame_group const expected_group[] = {
+        CPL_FRAME_GROUP_RAW,
+        CPL_FRAME_GROUP_RAW,
+        CPL_FRAME_GROUP_CALIB
+    };
+    cpl_frameset *frames = cpl_frameset_new();
+    for (i = 0; i < N; i++) {
+        cpl_frame *frame = cpl_frame_new();
+        cpl_frame_set_filename(frame, filename[i]);
+        cpl_frame_set_tag(frame, tag[i]);
+        cpl_frameset_insert(frames, frame);
+    }
+
+    /* Call the function */
+    if (cr2res_dfs_set_groups(frames) != 0) {
+        cpl_msg_error(fctid, "Function %s failed", test_subject);
+        cpl_errorstate_dump(prestate, CPL_FALSE, NULL);
+        cpl_frameset_delete(frames);
+        cpl_end();
+        exit(EXIT_FAILURE);       
+    }
     
-        for (i = 0; i < N; i++) {
-            cpl_frame *frame = cpl_frame_new();
-
-            cpl_frame_set_filename(frame, filename[i]);
-            cpl_frame_set_tag(frame, tag[i]);
-            cpl_frameset_insert(frames, frame);
-        }
-
-        /* Call the function */
-        if (cr2re_dfs_set_groups(frames) != 0) {
-            cpl_msg_error(fctid, "Function %s failed",
-                          test_subject);
+    /* Verify results */
+    for (i = 0; i < N; i++) {
+        cpl_frame *frame = cpl_frameset_get_frame(frames, i);
+        if (frame == NULL) {
+            cpl_msg_error(fctid, "Missing frame number %d", i);
             cpl_errorstate_dump(prestate, CPL_FALSE, NULL);
             cpl_frameset_delete(frames);
             cpl_end();
             exit(EXIT_FAILURE);       
         }
-        
-        /* Verify results */
-        for (i = 0; i < N; i++) {
-            cpl_frame *frame = cpl_frameset_get_frame(frames, i);
 
-            if (frame == NULL) {
-                cpl_msg_error(fctid, "Missing frame number %d",
-                              i);
-                cpl_errorstate_dump(prestate, CPL_FALSE, NULL);
-                cpl_frameset_delete(frames);
-                cpl_end();
-                exit(EXIT_FAILURE);       
-            }
-
-            if (cpl_frame_get_group(frame) != expected_group[i]) {
-                cpl_msg_error(fctid, "Frame number %d has group %s, "
-                              "%s expected",
-                              i,
-                              frame_group_to_string(cpl_frame_get_group(frame)),
-                              frame_group_to_string(expected_group[i]));
-                cpl_errorstate_dump(prestate, CPL_FALSE, NULL);
-                cpl_frameset_delete(frames);
-                cpl_end();
-                exit(EXIT_FAILURE);
-            }
+        if (cpl_frame_get_group(frame) != expected_group[i]) {
+            cpl_msg_error(fctid, "Frame number %d has group %s, %s expected",
+                    i, 
+                cr2res_test_frame_group_to_string(cpl_frame_get_group(frame)),
+                cr2res_test_frame_group_to_string(expected_group[i]));
+            cpl_errorstate_dump(prestate, CPL_FALSE, NULL);
+            cpl_frameset_delete(frames);
+            cpl_end();
+            exit(EXIT_FAILURE);
         }
-        
-        cpl_frameset_delete(frames);
     }
-    
+    cpl_frameset_delete(frames);
     return;
 }
 
 /*----------------------------------------------------------------------------*/
 /**
-  @brief    Unit tests of cr2re_dfs module
+  @brief    Unit tests of cr2res_dfs module
  */
 /*----------------------------------------------------------------------------*/
-
 int main(void)
 {
-#if defined CPL_VERSION_CODE && CPL_VERSION_CODE >= CPL_VERSION(4, 0, 0)
     cpl_init(CPL_INIT_DEFAULT);
-#else
-    cpl_init();
-#endif
 
-    test_set_groups();
+    cr2res_test_dfs_set_groups() ;
 
     cpl_end();
     exit(EXIT_SUCCESS);
 }
 
 /**@}*/
+
+/*----------------------------------------------------------------------------*/
+/**
+  @brief    Textual representation of CPL frame group
+  @param    group     to convert
+  @return   textual representation
+ */
+/*----------------------------------------------------------------------------*/
+static const char * cr2res_test_frame_group_to_string(cpl_frame_group group)
+{
+    const char * self = "";
+
+    switch(group) {
+        case CPL_FRAME_GROUP_RAW:
+            self = CPL_FRAME_GROUP_RAW_ID;
+            break;
+        case CPL_FRAME_GROUP_NONE:
+            self = "NONE";
+            break;
+        case CPL_FRAME_GROUP_CALIB:
+            self = CPL_FRAME_GROUP_CALIB_ID;
+            break;
+        case CPL_FRAME_GROUP_PRODUCT:
+            self = CPL_FRAME_GROUP_PRODUCT_ID;
+            break;
+        default:
+            break;
+    }
+
+    return self;
+}
