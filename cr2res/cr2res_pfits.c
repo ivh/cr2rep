e178d1a3f820ed78757f489bed57164c0ce928e5
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2017-06-27 07:10:58 +0000

    minor
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@204432 318c8218-2dab-440d-9263-6f70bac63e5b

--- cr2res/cr2res_pfits.c
+++ cr2res/cr2res_pfits.c
@@ -26,7 +26,7 @@
  -----------------------------------------------------------------------------*/
 
 #include <cpl.h>
-
+#include <math.h>
 #include "cr2res_pfits.h"
 
 /*----------------------------------------------------------------------------*/
@@ -74,7 +74,9 @@ int cr2res_pfits_get_order(
                best_number = i;
         }
     }
-
+    if (best_diff > 50.0)
+        cpl_msg_warn(__func__,"Order %d identified with large difference "
+                              "of %.1f pix",  best_number, best_diff);
     cpl_free(key_name) ;
     return best_number ;
 }
