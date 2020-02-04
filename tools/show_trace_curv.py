91bfae684b82dbe67f47466a12a76269d51fb86b
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2020-02-04 12:34:20 +0000

    plot tweaks
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@249973 318c8218-2dab-440d-9263-6f70bac63e5b

--- tools/show_trace_curv.py
+++ tools/show_trace_curv.py
@@ -3,8 +3,11 @@ import os, sys
 from astropy.io import fits
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from turbo_colormap import turbo_colormap_data
+Turbo = ListedColormap(turbo_colormap_data)
 
-def compare(fname_trace, fname_img=None, fname_spec=None):
+def compare(fname_trace, fname_img=None):
     """ compare img and trace """
     trace = fits.open(fname_trace)
     if fname_img:
@@ -12,11 +15,9 @@ def compare(fname_trace, fname_img=None, fname_spec=None):
         linecol = "w"
     else:
         linecol = "k"
-    if fname_spec:
-        spec = fits.open(fname_spec)
-
+    
     X = np.arange(2048)
-    FIG = plt.figure(figsize=(10, 3.5))
+    FIG = plt.figure(figsize=(15, 5))
 
     for i in [1, 2, 3]:
         ax = FIG.add_subplot(1, 3, i)
@@ -34,8 +35,9 @@ def compare(fname_trace, fname_img=None, fname_spec=None):
 
         if fname_img:
             imgdata = img[i].data
-            vmin, vmax = np.percentile(imgdata, (5, 95))
-            ax.imshow(imgdata, origin="lower", vmin = vmin, vmax=vmax)
+            vmin, vmax = np.percentile(imgdata, (5, 99))
+            ax.imshow(imgdata, origin="lower", vmin = vmin, vmax=vmax,
+                cmap='viridis')
 
         for t in tdata:
             upper = t["Upper"]
@@ -61,17 +63,6 @@ def compare(fname_trace, fname_img=None, fname_spec=None):
             i2 = tdata[tdata["order"] == order]["All"]
             coeff = [np.interp(0.5, i1, i2[:, k]) for k in range(i2.shape[1])]
 
-            if len(wave) == 2:
-                a,b = wave
-                c=0
-            elif len(wave) > 2:
-                a, b, c = wave
-            xw = np.polyval(wave[::-1], np.arange(2048))
-            x = [(np.sqrt(-4*a*c + b**2 + 4 * c * w) - b) / (2*c) for w in np.arange(np.floor(xw.min()), np.ceil(xw.max()))]
-            x = np.clip(np.asarray(x), 0, 2048)
-            idx = x.astype(int)
-            plt.vlines(x, lower[idx], upper[idx], colors="r")
-
 
             for i in range(10, 2048, 100):
                 ew = [int(middle[i] - lower[i]), int(upper[i] - middle[i])]
@@ -91,7 +82,7 @@ def compare(fname_trace, fname_img=None, fname_spec=None):
                 for j, yt in enumerate(y):
                     x[j] = i + yt * b + yt ** 2 * c
                 y += middle[i]
-                plt.plot(x, y, "--r")
+                plt.plot(x, y, "-"+linecol)
 
             if np.isnan(middle[1024]):
                 continue
