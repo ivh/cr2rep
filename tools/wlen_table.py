0dca56a8048b5aa13b4faf849d3c1fb56a5b9d4a
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-01-25 15:51:37 +0000

    add tools/wlen_table.py which prints start and end WL from a TW file
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@260443 318c8218-2dab-440d-9263-6f70bac63e5b

---
+++ tools/wlen_table.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os, sys
+from astropy.io import fits
+import numpy as np
+
+
+X = np.arange(2048)+1
+
+for twn in sys.argv[1:]:
+    print('#File: %s'%twn)
+    print('#Order Detector XC StartWL EndWL')
+    with fits.open(twn) as twf:
+        for order in [9,8,7,6,5,4,3,2,1]:      
+            for det in [1,2,3]:
+                tw = twf['CHIP%d.INT1'%det].data
+                if tw is None: continue
+                h = twf['CHIP%d.INT1'%det].header
+                xc = h.get('ESO QC WAVE BESTXCORR-%02d-01'%order, 0.0)
+                mask = tw['Order']==order
+                try:
+                    p = tw[mask]['Wavelength'][0][::-1]
+                except:
+                    print('f')
+                    continue
+                poly = np.poly1d(p)
+                #deriv = poly.deriv()
+                print(order, det, '%.1f%%'%xc, '%.3f'%poly(1), '%.3f'%poly(2048))
+                
