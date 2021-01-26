1c566d82d26f3dd71c7d40662c2619457f1dc3b7
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-01-26 15:15:38 +0000

    better default parameter values for cal_flat and util_trace
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@260470 318c8218-2dab-440d-9263-6f70bac63e5b

--- recipes/cr2res_cal_flat.c
+++ recipes/cr2res_cal_flat.c
@@ -303,7 +303,7 @@ static int cr2res_cal_flat_create(cpl_plugin * plugin)
 
     p = cpl_parameter_new_value("cr2res.cr2res_cal_flat.trace_min_cluster",
             CPL_TYPE_INT, "size in pixels of the smallest allowed cluster",
-            "cr2res.cr2res_cal_flat", 150000);
+            "cr2res.cr2res_cal_flat", 250000);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "trace_min_cluster");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
@@ -324,7 +324,7 @@ static int cr2res_cal_flat_create(cpl_plugin * plugin)
 
     p = cpl_parameter_new_value("cr2res.cr2res_cal_flat.trace_threshold",
             CPL_TYPE_DOUBLE, "Detection Threshold",
-            "cr2res.cr2res_cal_flat", 1100.0);
+            "cr2res.cr2res_cal_flat", 3.0);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "trace_threshold");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
@@ -346,13 +346,13 @@ static int cr2res_cal_flat_create(cpl_plugin * plugin)
 
     p = cpl_parameter_new_value("cr2res.cr2res_cal_flat.extract_oversample",
             CPL_TYPE_INT, "factor by which to oversample the extraction",
-            "cr2res.cr2res_cal_flat", 3);
+            "cr2res.cr2res_cal_flat", 2);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "extract_oversample");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
 
     p = cpl_parameter_new_value("cr2res.cr2res_cal_flat.extract_swath_width",
-            CPL_TYPE_INT, "The swath width", "cr2res.cr2res_cal_flat", 24);
+            CPL_TYPE_INT, "The swath width", "cr2res.cr2res_cal_flat", 600);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "extract_swath_width");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
