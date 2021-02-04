e9857c0a46dafb7e567da62851a8af2d169ba46d
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-02-04 16:23:44 +0000

    dummy test
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@260790 318c8218-2dab-440d-9263-6f70bac63e5b

--- cr2res/tests/cr2res_detlin-test.c
+++ cr2res/tests/cr2res_detlin-test.c
@@ -31,7 +31,7 @@
 #include <cpl.h>
 #include <hdrl.h>
 #include <cr2res_dfs.h>
-#include <cr2res_pol.h>
+#include <cr2res_detlin.h>
 
 #define CR2RES_DETECTOR_SIZE            2048
 
@@ -65,6 +65,25 @@ static void test_cr2res_detlin_compute(){
         cpl_polynomial      **  fitted,
         cpl_vector          **  error) */
 
+  int n = 100;
+  double aduPsec = 1000.1;
+  cpl_size max_degree=3;
+  cpl_vector    *   dits  = cpl_vector_new(n);
+  cpl_vector    *   values = cpl_vector_new(n);
+  cpl_polynomial      *  fitted;
+  cpl_vector          *  error;
+
+
+
+
+  cr2res_detlin_compute(dits, values, max_degree, &fitted, &error);
+
+  //cpl_vector_dump(fitted);
+
+  cpl_vector_delete(dits);
+  cpl_vector_delete(values);
+  if (fitted != NULL) cpl_vector_delete(error);
+  if (error != NULL) cpl_polynomial_delete(fitted);
   return ;
 }
 
