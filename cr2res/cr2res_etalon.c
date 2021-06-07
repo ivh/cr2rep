863bcc6b95ef36363517296d90ccbb0f6f13e335
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2021-06-07 10:34:00 +0200

    fix order number in etalon

--- cr2res/cr2res_etalon.c
+++ cr2res/cr2res_etalon.c
@@ -907,7 +907,7 @@ cpl_polynomial * cr2res_etalon_wave_2d(
             k++;
         }
     }
-    
+
     result = cpl_polynomial_new(2);
     degree_2d[0] = degree_x ;
     degree_2d[1] = degree_y ;
