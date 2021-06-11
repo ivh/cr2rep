4979607237df38e58d76d4551a8ffcd6a4428f6f
Ansgar Wehrhahn <31626864+AWehrhahn@users.noreply.github.com>
2021-06-11 13:27:29 +0200

    fix merge

--- cr2res/cr2res_etalon.c
+++ cr2res/cr2res_etalon.c
@@ -985,9 +985,6 @@ cpl_polynomial * cr2res_etalon_wave_2d_nikolai(
     cpl_table * lines_diagnostics_loc;
     double pix_pos, lambda_cat, lambda_meas, line_width, line_intens, fit_error;
 
-    cpl_table * lines_diagnostics_loc;
-    double pix_pos, lambda_cat, lambda_meas, line_width, line_intens, fit_error;
-
     /* Check Inputs */
     if (spectra==NULL || spectra_err==NULL || wavesol_init==NULL ||
             orders==NULL)
