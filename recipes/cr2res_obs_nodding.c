6a2f7b47384182894b10a3fa20302477d0bee382
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-10-23 18:33:32 +0000

    change 2 default params in obs_nodding, re-instantiate the Execter in Wkf
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@267796 318c8218-2dab-440d-9263-6f70bac63e5b

--- recipes/cr2res_obs_nodding.c
+++ recipes/cr2res_obs_nodding.c
@@ -303,7 +303,7 @@ static int cr2res_obs_nodding_create(cpl_plugin * plugin)
 
     p = cpl_parameter_new_value("cr2res.cr2res_obs_nodding.extract_oversample",
             CPL_TYPE_INT, "factor by which to oversample the extraction",
-            "cr2res.cr2res_obs_nodding", 5);
+            "cr2res.cr2res_obs_nodding", 7);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "extract_oversample");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
@@ -324,7 +324,7 @@ static int cr2res_obs_nodding_create(cpl_plugin * plugin)
     p = cpl_parameter_new_value("cr2res.cr2res_obs_nodding.extract_smooth_slit",
             CPL_TYPE_DOUBLE,
             "Smoothing along the slit",
-            "cr2res.cr2res_obs_nodding", 2.0);
+            "cr2res.cr2res_obs_nodding", 1.0);
     cpl_parameter_set_alias(p, CPL_PARAMETER_MODE_CLI, "extract_smooth_slit");
     cpl_parameter_disable(p, CPL_PARAMETER_MODE_ENV);
     cpl_parameterlist_append(recipe->parameters, p);
