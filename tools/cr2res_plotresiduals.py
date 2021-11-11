b3bf9381c4d6f2f798be90bf55a373e4afc46001
marquart <marquart@318c8218-2dab-440d-9263-6f70bac63e5b>
2021-11-11 17:56:58 +0000

    add velocity scale to tools/cr2res_plotresiduals.py
    git-svn-id: http://svnhq2.hq.eso.org/p2/trunk/Pipelines/cr2re/cr2rep@268139 318c8218-2dab-440d-9263-6f70bac63e5b

--- tools/cr2res_plotresiduals.py
+++ tools/cr2res_plotresiduals.py
@@ -3,9 +3,9 @@ import os, sys
 import numpy as np
 from matplotlib import pyplot as plt
 from astropy.io import fits
-from scipy.constants import speed_of_light as c
+from scipy.constants import speed_of_light as sol
 
-fig = plt.figure()
+fig = plt.figure(figsize=(7,5))
 ax = fig.subplots()
 try:
     vmi,vma=[float(v) for v in sys.argv[1].split(',')]
@@ -14,9 +14,16 @@ except:
     vmi,vma=None,None
     offs=0
 
+def dlam2vel(lam0):
+    return lambda dlam: dlam/lam0*sol
+
+def vel2dlam(lam0):
+    return lambda vel: vel/sol*lam0
+
 for filename in sys.argv[1+offs:]:
     f = fits.open(filename)
     sett = f[0].header['HIERARCH ESO INS WLEN ID']
+    cwlen = f[0].header['HIERARCH ESO INS WLEN CWLEN']
     for d in [1,2,3]:
         dat = f['CHIP%d.INT1'%d].data
         xoff = (d-2)*0.2
@@ -29,10 +36,15 @@ for filename in sys.argv[1+offs:]:
     ax.set_title('Residual delta-lambda, %s'%sett)
     ax.legend(loc='upper right')
     ax.set_xlabel('Order #')
-    ax.set_ylabel('nm')
+    ax.set_ylabel(r'$\Delta \lambda$ [nm]')
     ax.set_xticks(np.arange(9)+1)
     if vmi:
         ax.set(ylim=(vmi,vma))
+
+    secax = ax.secondary_yaxis('right',functions=(dlam2vel(cwlen),vel2dlam(cwlen)))
+    secax.set_ylabel(r'$\Delta v$ [m/s]')
+
+    fig.tight_layout()
     outf = filename.replace('.fits','_resid.png')
     fig.savefig(outf)
     ax.clear()
